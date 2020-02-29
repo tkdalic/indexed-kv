1bbc540d64231ebaf7f24e57b1aafe0827cef6bf
tkdalic <himagami126@gmail.com>
2020-02-29 19:14:13 +0900

    add: sample svelteによる実装

--- sample/src/App.svelte
+++ sample/src/App.svelte
@@ -1,29 +1,75 @@
 <script>
-  export let name;
+  import { IndexedKv } from "../../dist/index";
+  const indexedKV = new IndexedKv();
+  let inputKey;
+  let inputValue;
+  let outputValue = "";
+
+  async function set() {
+    await indexedKV.set(inputKey, inputValue);
+    get();
+  }
+
+  function get() {
+    indexedKV.get(inputKey).then(value => (outputValue = value));
+  }
 </script>
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
+  .main {
+    width: 100%;
+    max-width: 500px;
+    margin: auto;
+  }
+  .indexed-kv {
+    display: flex;
+    flex-direction: column;
   }
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
+  .input {
+    display: flex;
   }
 
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
+  .input__label {
+    display: flex;
+    align-items: center;
+  }
+
+  .input__text {
+    flex-grow: 1;
   }
 </style>
 
-<main>
-  <h1>Hello {name}!</h1>
+<main class="main">
+  <h1>Indexed KV sample</h1>
+  <div class="indexed-kv">
+    <div class="indexed-kv__key input">
+      <div class="input__label">key:</div>
+      <input
+        class="input__text"
+        type="text"
+        bind:value={inputKey}
+        on:change={get} />
+    </div>
+    <div class="indexed-kv__value input">
+      <div class="input__label">value:</div>
+      <input class="input__text" type="text" bind:value={inputValue} />
+    </div>
+    <button class="indexed-kv__button" on:click={set}>反映</button>
+    <div class="indexed-kv__outpupt">indexedDB value: {outputValue}</div>
+  </div>
+
+  <p class="description">
+    indexed KV のサンプルです。
+    indexed KV は、indexedDBを Key Value Store
+    のように扱うライブラリです。
+    <br />
+    keyを変更するごとに、indexedDBから該当するキーを探し、
+    あればindexed-db valueを返却します。
+    <br />
+    キーがなければnullとなります。
+    <br />
+    <br />
+    また、反映を押すことで、該当のkeyのvalueを更新します。
+  </p>
 </main>
